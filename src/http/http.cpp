e57ed7a4dc6502b61cfbb4a24057bf5232193346
BangL <henno.rickowski@gmail.com>
2024-10-01 18:54:23 +0200

    revised curl/openssl once more. this should finally solve the sporadic startup issues

--- src/http/http.cpp
+++ src/http/http.cpp
@@ -6,6 +6,7 @@
 
 #include <algorithm>
 #include <thread>
+#include <openssl/ssl.h>
 
 namespace pd2hook
 {
@@ -18,22 +19,50 @@ namespace pd2hook
 
 		struct HTTPProgressNotification
 		{
-			HTTPItem *ourItem;
+			HTTPItem* ourItem;
 			long byteProgress;
 			long byteTotal;
 		};
 	}
 
-	static HTTPManager *s_httpSingleton = nullptr; // exit crash fix
+	static HTTPManager* s_httpSingleton = nullptr; // exit crash fix
 
-	static std::mutex curlLock;
+	CURLPool::CURLPool(size_t poolSize) {
+		for (size_t i = 0; i < poolSize; ++i) {
+			CURL* curl = curl_easy_init();
+			if (curl) {
+				pool.push_back(curl);
+			}
+		}
+	}
+
+	CURLPool::~CURLPool() {
+		for (auto& curl : pool) {
+			curl_easy_cleanup(curl);
+		}
+	}
+
+	CURL* CURLPool::acquire() {
+		std::lock_guard<std::mutex> lock(poolMutex);
+		if (!pool.empty()) {
+			CURL* curl = pool.back();
+			pool.pop_back();
+			return curl;
+		}
+		return curl_easy_init();
+	}
+
+	void CURLPool::release(CURL* curl) {
+		std::lock_guard<std::mutex> lock(poolMutex);
+		pool.push_back(curl);
+	}
 
 	using HTTPProgressNotificationPtr = std::unique_ptr<HTTPProgressNotification>;
 	using HTTPItemPtr = std::unique_ptr<HTTPItem>;
 	PD2HOOK_REGISTER_EVENTQUEUE(HTTPProgressNotificationPtr, HTTPProgressNotification)
 		PD2HOOK_REGISTER_EVENTQUEUE(HTTPItemPtr, HTTPItem)
 
-		void lock_callback(int mode, int type, const char *file, int line)
+		void lock_callback(int mode, int type, const char* file, int line)
 	{
 		if (mode & CRYPTO_LOCK)
 		{
@@ -46,20 +75,24 @@ namespace pd2hook
 	}
 
 	HTTPManager::HTTPManager()
-		: m_bLocksInit(false)
+		: curlPool(10), m_bLocksInit(false)
 	{
 		// Curl Init
 		curl_global_init(CURL_GLOBAL_ALL);
+		if (OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS, nullptr) != 1) {
+			PD2HOOK_LOG_LOG("OpenSSL initialization failed.");
+		}
 		PD2HOOK_LOG_LOG("CURL_INITD");
 	}
 
 	HTTPManager::~HTTPManager()
 	{
 		CRYPTO_set_locking_callback(NULL);
+		CRYPTO_set_id_callback(NULL);
 		PD2HOOK_LOG_LOG("CURL CLOSED");
 		curl_global_cleanup();
 
-		std::for_each(threadList.begin(), threadList.end(), [](const std::unique_ptr<std::thread> &t)
+		std::for_each(threadList.begin(), threadList.end(), [](const std::unique_ptr<std::thread>& t)
 			{ t->join(); });
 	}
 
@@ -69,10 +102,13 @@ namespace pd2hook
 		numLocks = CRYPTO_num_locks();
 		openssl_locks.reset(new std::mutex[numLocks]);
 		CRYPTO_set_locking_callback(lock_callback);
+		CRYPTO_set_id_callback([]() -> unsigned long {
+			return std::hash<std::thread::id>{}(std::this_thread::get_id());
+			});
 		m_bLocksInit = true;
 	}
 
-	HTTPManager *HTTPManager::GetSingleton()
+	HTTPManager* HTTPManager::GetSingleton()
 	{
 
 		// exit crash fix
@@ -102,15 +138,15 @@ namespace pd2hook
 		openssl_locks[lockno].unlock();
 	}
 
-	size_t write_http_data(char *ptr, size_t size, size_t nmemb, void *data)
+	size_t write_http_data(char* ptr, size_t size, size_t nmemb, void* data)
 	{
 		std::string newData = std::string(ptr, size * nmemb);
-		HTTPItem *mainItem = (HTTPItem *)data;
+		HTTPItem* mainItem = (HTTPItem*)data;
 		mainItem->httpContents += newData;
 		return size * nmemb;
 	}
 
-	size_t write_data_to_file(void *ptr, size_t size, size_t nmemb, FILE *stream)
+	size_t write_data_to_file(void* ptr, size_t size, size_t nmemb, FILE* stream)
 	{
 		size_t written = fwrite(ptr, size, nmemb, stream);
 		return written;
@@ -119,14 +155,14 @@ namespace pd2hook
 	void run_http_progress_event(std::unique_ptr<HTTPProgressNotification> ourNotify)
 	{
 		PD2HOOK_TRACE_FUNC;
-		HTTPItem *ourItem = ourNotify->ourItem;
+		HTTPItem* ourItem = ourNotify->ourItem;
 		ourItem->progress(ourItem->data, ourNotify->byteProgress, ourNotify->byteTotal);
 	}
 
-	int http_progress_call(void *clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow)
+	int http_progress_call(void* clientp, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow)
 	{
 		PD2HOOK_TRACE_FUNC;
-		HTTPItem *ourItem = (HTTPItem *)clientp;
+		HTTPItem* ourItem = (HTTPItem*)clientp;
 		if (!ourItem->progress)
 			return 0;
 		if (dltotal == 0 || dlnow == 0)
@@ -153,40 +189,38 @@ namespace pd2hook
 		ourItem->call(ourItem->data, ourItem->httpContents);
 	}
 
-	void launch_thread_http(HTTPItem *raw_item)
+	void launch_thread_http(HTTPItem* raw_item)
 	{
 		PD2HOOK_TRACE_FUNC;
 		std::unique_ptr<HTTPItem> item(raw_item);
 
+		CURLPool& curlPool = HTTPManager::GetSingleton()->GetCURLPool();
+
+		CURL* curl = curlPool.acquire();
+
+		curl_easy_setopt(curl, CURLOPT_URL, item->url.c_str());
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+		curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30L);
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 900L);
+		curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 30L);
+		curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 1000L);
+
+		if (item->progress)
 		{
-			std::lock_guard<std::mutex> lock(curlLock);
-			CURL* curl;
-			curl = curl_easy_init();
-
-			curl_easy_setopt(curl, CURLOPT_URL, item->url.c_str());
-			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-
-			curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30L);
-			curl_easy_setopt(curl, CURLOPT_TIMEOUT, 900L);
-			curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 30L);
-			curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 1000L);
-
-			if (item->progress)
-			{
-				curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, http_progress_call);
-				curl_easy_setopt(curl, CURLOPT_XFERINFODATA, item.get());
-				curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
-			}
-
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_http_data);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, item.get());
-
-			curl_easy_perform(curl);
-
-			curl_easy_cleanup(curl);
+			curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, http_progress_call);
+			curl_easy_setopt(curl, CURLOPT_XFERINFODATA, item.get());
+			curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
 		}
+
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_http_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, item.get());
+
+		curl_easy_perform(curl);
+
+		curlPool.release(curl);
 
 		GetHTTPItemQueue().AddToQueue(run_http_event, std::move(item));
 	}
@@ -208,31 +242,29 @@ namespace pd2hook
 	{
 		FILE* fp = fopen(strDestination.data(), "wb");
 
-		{
-			std::lock_guard<std::mutex> lock(curlLock);
-			CURL* curl;
-			curl = curl_easy_init();
+		CURLPool& curlPool = HTTPManager::GetSingleton()->GetCURLPool();
 
-			auto ver = curl_version_info(CURLVERSION_NOW);
-			auto test = ver->libssh_version;
+		CURL* curl = curlPool.acquire();
 
-			curl_easy_setopt(curl, CURLOPT_URL, strUrl.data());
-			curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-			curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+		auto ver = curl_version_info(CURLVERSION_NOW);
+		auto test = ver->libssh_version;
 
-			curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30L);
-			curl_easy_setopt(curl, CURLOPT_TIMEOUT, 900L);
-			curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 30L);
-			curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 1000L);
+		curl_easy_setopt(curl, CURLOPT_URL, strUrl.data());
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_to_file);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+		curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30L);
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 900L);
+		curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 30L);
+		curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 1000L);
 
-			curl_easy_perform(curl);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_to_file);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
 
-			curl_easy_cleanup(curl);
-		}
+		curl_easy_perform(curl);
+
+		curlPool.release(curl);
 
 		if (fp)
 			fclose(fp);
--- src/http/http.h
+++ src/http/http.h
@@ -7,11 +7,13 @@
 #include <list>
 #include <memory>
 #include <string_view>
+#include <vector>
+#include <curl/curl.h>
 
 namespace pd2hook
 {
-	typedef void (*HTTPCallback)(void *data, std::string &urlContents);
-	typedef void (*HTTPProgress)(void *data, long progress, long total);
+	typedef void (*HTTPCallback)(void* data, std::string& urlContents);
+	typedef void (*HTTPProgress)(void* data, long progress, long total);
 
 	struct HTTPItem
 	{
@@ -19,10 +21,26 @@ namespace pd2hook
 		HTTPProgress progress = nullptr;
 		std::string url;
 		std::string httpContents;
-		void *data = nullptr;
+		void* data = nullptr;
 
 		long byteprogress = 0;
 		long bytetotal = 0;
+	};
+
+	class CURLPool
+	{
+	public:
+		CURLPool(size_t poolSize);
+
+		~CURLPool();
+
+		CURL* acquire();
+
+		void release(CURL* curl);
+
+	private:
+		std::mutex poolMutex;
+		std::vector<CURL*> pool;
 	};
 
 	class HTTPManager
@@ -37,7 +55,7 @@ namespace pd2hook
 
 		inline bool AreLocksInit() const { return m_bLocksInit; }
 
-		static HTTPManager *GetSingleton();
+		static HTTPManager* GetSingleton();
 		static void Destroy(); // exit crash fix
 
 		void SSL_Lock(int lockno);
@@ -47,7 +65,9 @@ namespace pd2hook
 
 		void DownloadFile(std::string_view strUrl, std::string_view strDestination);
 
+		CURLPool& GetCURLPool() { return curlPool; };
 	private:
+		CURLPool curlPool;
 		std::unique_ptr<std::mutex[]> openssl_locks;
 		int numLocks = 0;
 		std::list<std::unique_ptr<std::thread>> threadList;
